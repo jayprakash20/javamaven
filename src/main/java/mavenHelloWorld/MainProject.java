4e60b484eec28bd44be2a63f31e691acf73597be
jayprakash20 <64167041+jayprakash20@users.noreply.github.com>
2020-05-21 21:31:03 +1000

    Update MainProject.java

--- src/main/java/mavenHelloWorld/MainProject.java
+++ src/main/java/mavenHelloWorld/MainProject.java
@@ -4,6 +4,7 @@ public class MainProject {
 
 	public static void main(String[] args) {
 		System.out.println("This is my Hello Word Application");
+		System.out.println("This is my Hello Word Application");
 
 	}
 
